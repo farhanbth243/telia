98b74a31040d41fb90fbc7970666cd96d43d80dd
farhanbth243 <127053952+farhanbth243@users.noreply.github.com>
2023-04-02 19:18:54 +0200

    Update UserRepository.java

--- src/main/java/com/telia/repository/UserRepository.java
+++ src/main/java/com/telia/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.telia.repository;
 
 import com.telia.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-    Optional<User> findByPersonalNumber(Long personalNumber);
+    Optional<User> findByPersonalNumber(String personalNumber);
+
+    List<User> findAllByFullNameContainingIgnoreCaseAndPersonalNumber(String name, String personalNumber, Sort sort);
+
+    List<User> findAllByFullNameContainingIgnoreCase(String name, Sort sort);
 }
